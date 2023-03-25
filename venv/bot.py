5e2d80518d59642650c9f98f0672fed7c1dca9b5
chanikarnock <chanikarnock@gmail.com>
2023-03-25 16:34:04 +0700

    improve code readability

--- venv/bot.py
+++ venv/bot.py
@@ -8,6 +8,7 @@ is_ready = False
 round_count = 0
 current_index = 0
 
+
 # Send messages
 async def send_message(message, text):
     try:
@@ -24,8 +25,7 @@ async def reply_message(message, text):
 
 
 def run_discord_bot():
-    TOKEN = '' # place your token here
-    intents = discord.Intents.default()
+    TOKEN = ''  # place your token here
     intents.message_content = True
     client = discord.Client(intents=intents)
 
@@ -43,12 +43,13 @@ def run_discord_bot():
         channel = str(message.channel)
         print(f"{username} said: '{user_message}' ({channel})")
         if user_message[:3] == '!cb':
-            u_msg_list = user_message.split()
-            command = u_msg_list[1]
+            user_message_list = user_message.split()
+            command = user_message_list[1]
             if command == "set":
                 if not is_active_battle:
                     is_active_battle = True
-                    await send_message(message, "**=== The encounter is about to start! ===**\n use `!cb join` to join!")
+                    await send_message(message,
+                                       "**=== The encounter is about to start! ===**\n use `!cb join` to join!")
                 else:
                     await send_message(message, "Encounter is already running")
                     return
@@ -56,25 +57,7 @@ def run_discord_bot():
                 if not is_active_battle:
                     await send_message(message, "No encounter running")
                     return
-                name = u_msg_list[2]
-                roll_list = []
-                if 'd' in u_msg_list[3]:
-                    roll_list, roll_result = dice(u_msg_list[3])
-                else:
-                    roll_result = u_msg_list[3]
-                combatant = {
-                    "name": name,
-                    "init": int(roll_result),
-                    "user": username,
-                    "is_active": False
-                }
-                combat_list.append(combatant)
-                combat_list.sort(key=lambda x: x["init"], reverse=True)
-                if roll_list:
-                    await reply_message(message, f"**{name}** join the encounter with ({u_msg_list[3]}) => **{roll_result}**!")
-                else:
-                    await reply_message(message, f"**{name}** join the encounter with **{roll_result}**!")
-                return
+                join_combat(message, user_message_list, combat_list)
             elif command == "start" or command == "s":
                 if not is_active_battle:
                     await send_message(message, "No encounter running")
@@ -82,12 +65,11 @@ def run_discord_bot():
                 combat_list[0]["is_active"] = True
                 await show_list(message, combat_list, round_count)
                 is_ready = True
-                return
             elif command == "next" or command == "n":
                 if not is_active_battle:
                     await send_message(message, "No encounter running")
                     return
-                if current_index < len(combat_list)-1:
+                if current_index < len(combat_list) - 1:
                     combat_list[current_index]["is_active"] = False
                     current_index += 1
                     combat_list[current_index]["is_active"] = True
@@ -97,54 +79,74 @@ def run_discord_bot():
                     current_index = 0
                     combat_list[current_index]["is_active"] = True
                 await show_list(message, combat_list, round_count)
-                return
             elif command == "remove" or command == "r":
                 if not is_active_battle:
                     await send_message(message, "No encounter running")
                     return
-                remove_index = int(u_msg_list[2]) - 1
+                remove_index = int(user_message_list[2]) - 1
                 await send_message(message, f"Remove **{combat_list[remove_index]['name']}** from the encounter!")
                 combat_list.pop(remove_index)
-                return
             elif command == "end" or command == "e":
                 if not is_active_battle:
-                    await send_message(message, "No encounter running")
+                    await send_message(message, "No encounter running"
+                    return
                 else:
                     is_active_battle = False
                     is_ready = False
                     await send_message(message, "**=== THE ENCOUNTER IS END! ===**")
-                return
             elif command == "list":
                 await show_list(message, combat_list, round_count)
             elif command == "roll":
-                dice_arr, total = dice(u_msg_list[2])
+                dice_arr, total = roll_dice(user_message_list[2])
                 await reply_message(message, f"You rolled: {dice_arr} \n**★ SUM => {total}**")
+
     client.run(TOKEN)
 
-async def show_list(message, combat_list, round_count):
+
+async def join_combat(message, user_message_list, combat_list):
+    name = user_message_list[2]
+    roll_list = []
+    dice_param = ''
+    if 'd' in user_message_list[3]:
+        roll_list, roll_result = roll_dice(user_message_list[3])
+        dice_param = user_message_list[3]
+    else:
+        roll_result = user_message_list[3]
+    combatant = {
+        "name": name,
+        "init": int(roll_result),
+        "user": username,
+        "is_active": False
+    }
+    combat_list.append(combatant)
+    combat_list.sort(key=lambda x: x["init"], reverse=True)
+    if roll_list:
+        await reply_message(message, f"**{name}** join the encounter with ({dice_param}) => **{roll_result}**!")
+    else:
+        await reply_message(message, f"**{name}** join the encounter with **{roll_result}**!")
+
+
+async def show_list(message, participants, round_count):
     try:
         title = f"Battle List - Round {round_count}"
-        desctp = ""
-        loop_count = 0
-        for c in combat_list:
-            desctp += f"{loop_count+1}. {c['name']}"
-            if c["is_active"]:
-                desctp += " ⚔️⚔️⚔️ \n"
+        description = ""
+        for index, participant in enumerate(participants):
+            description += f"{index + 1}. {participant['name']}"
+            if participant['is_active']:
+                description += " ⚔️⚔️⚔️ \n"
             else:
-                desctp += "\n"
-            loop_count += 1
-        embed = discord.Embed(title=title, description=desctp, color=0xdb0000)
+                description += "\n"
+        embed = discord.Embed(title=title, description=description, color=0xdb0000)
         await message.channel.send(embed=embed)
-    except Exception as e:
+    except discord.DiscordException as e:
         print(e)
 
 
-def dice(dice_string):
+def roll_dice(dice_string):
     pattern = re.compile(r'(\d*)d(\d+)')
     matches = pattern.finditer(dice_string)
     roll_list = []
     total = 0
-    round_count = 0
     for match in matches:
         dice_count = int(match.group(1) or '1')
         dice_sides = int(match.group(2))
@@ -152,13 +154,10 @@ def dice(dice_string):
         roll_list.append(roll)
         result = sum(roll)
         total += result
-        round_count+=1
+
     modifier = []
     modifier = re.findall(r'([+-](?!\d*d)\d*\w*)', dice_string)
     if modifier:
         for mod in modifier:
             total = eval(f"{total}{mod}")
     return roll_list, total
-
-
-
